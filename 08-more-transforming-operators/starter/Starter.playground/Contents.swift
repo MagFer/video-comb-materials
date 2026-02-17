4309a8a3a54d56ea2b14bf13051112e5ff3ecc1b
Ian Magarzo Fernandez <ian.magfer@gmail.com>
2026-02-17 20:48:05 +0100

    08. More transforming operators

--- 08-more-transforming-operators/starter/Starter.playground/Contents.swift
+++ 08-more-transforming-operators/starter/Starter.playground/Contents.swift
@@ -3,7 +3,59 @@ import Combine
 
 var subscriptions = Set<AnyCancellable>()
 
-<#Add your code here#>
+example(of: "scan") {
+  var dailyGainLoos: Int { .random(in: -10...10)}
+  
+  let august2019 = (0..<22)
+    .map({ _ in dailyGainLoos})
+    .publisher
+  
+  august2019
+    .scan(50) { latest, current in
+      max(0, latest + current)
+    }
+    .sink(receiveValue: { _ in })
+    .store(in: &subscriptions)
+}
+
+example(of: "flat map") {
+  let charlotte = Chatter(
+    name: "Charlotte", 
+    message: "Hi, I'm Charlotte"
+  )
+  
+  let james = Chatter(
+    name: "James",
+    message: "Hi, I'm James!"
+  )
+  
+  let chat = CurrentValueSubject<Chatter, Never>(charlotte)
+  
+  /*
+  chat
+    .flatMap({ chatter in
+      chatter.message
+    })
+    .sink(receiveValue: { value in
+      print(value)
+    })
+    .store(in: &subscriptions)
+  */
+  
+  chat
+    .flatMap { $0.message }
+    .sink(receiveValue: { print($0) })
+    .store(in: &subscriptions)
+  
+  charlotte.message.value = "Charlotte: How's it going?"
+  chat.value = james
+  
+  james.message.value = "James: Doing great! You?"
+  charlotte.message.value = "Charlotte: I'm doing fine, thanks."
+  
+  chat
+    .send(completion: .finished)
+}
 
 /// Copyright (c) 2019 Razeware LLC
 ///
